ddb3c9ed4a1e11474275884d230c329d1416e580
Yuyang Xie <yxie222@wisc.edu>
2021-10-22 17:04:02 -0500

    addtimestamp

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 import requests
 import os
 import json
+from datetime import datetime
 
 # To set your environment variables in your terminal run the following line:
-# export 'BEARER_TOKEN'='<your_bearer_token>'
+# export 'BEARER_TOKEN'='<your_token_here>'
 bearer_token = os.environ.get("BEARER_TOKEN")
 
 
 def create_url():
-    return "https://api.twitter.com/2/tweets/sample/stream"
+    return "https://api.twitter.com/2/tweets/search/recent?query=from%3Atwitterdev&tweet.fields=created_at"
 
 
 def bearer_oauth(r):
@@ -27,6 +28,7 @@ def connect_to_endpoint(url):
     for response_line in response.iter_lines():
         if response_line:
             json_response = json.loads(response_line)
+            # created_at = datetime.strptime(response_line[0]['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
             print(json.dumps(json_response, indent=4, sort_keys=True))
     if response.status_code != 200:
         raise Exception(
