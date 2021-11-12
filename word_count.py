d8e06af68cf4c45dec965cad54c2cfd7160469fd
calvinc0123 <chen2328@wisc.edu>
2021-11-12 17:33:56 -0600

    Milestone1_partb

--- word_count.py
+++ word_count.py
@@ -1,23 +1,60 @@
+
 """
+File: word_count.py
+Name: Group
+---------------------------
 This file compute frequencies of words and phrases 
 by using argparse
 """
 
 import argparse
+import itertools
+import collections
 
-# create args
-# parser = argparse.ArgumentParser()
-# parser.add_argument('word', metavar='word', type=str, help='enter your word')
-# args = parser.parse_args()
+FILENAME = 'tweets.txt'
+WORD_DICT = {}
 
-# word = args.word
 
-word = input('Enter word: ')
+def count_freq_word(line):
+	"""
+	Count the frequency of the words
+	"""
+	word_ls = line[1].split(" ")  
+	for word in word_ls:
 
-with open('tweet4.txt', 'r') as file:
-    data = file.read().replace('\n', '')
+		if word not in WORD_DICT:
+			if '@' in word:
+				pass
+			else:
+				WORD_DICT[word] = 1   
+		else:
+			WORD_DICT[word] += 1
 
-c = data.count(word)
 
-print(word)
-print(c)
+
+def read_file(filename, word):
+	"""
+	Read the file 
+	"""
+	with open(filename, 'r') as f:
+		for line in f:
+			line = line.strip()
+			line_l = line.split(",")
+			count_freq_word(line_l)
+
+def main():
+	# create args
+	parser = argparse.ArgumentParser()
+	# parser.add_argument('word', metavar='word', type=str, help='enter your word')
+	# parser.add_argument('sentence', help='enter your sentence')
+	parser.add_argument('-w','--word', help='enter your word', required=False)
+	args = parser.parse_args()
+	
+	word = args.word
+	
+	read_file(FILENAME, word)
+	print(WORD_DICT[word])
+
+if __name__ == '__main__':
+	main()	
+
