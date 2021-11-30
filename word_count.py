f5d6c85c3a09b1d0501c0e152f9d1c9de1547005
calvinc0123 <chen2328@wisc.edu>
2021-11-30 14:51:40 -0600

    M1 PartB

--- word_count.py
+++ word_count.py
@@ -10,9 +10,11 @@ by using argparse
 import argparse
 import itertools
 import collections
+import pandas as pd
 # ngram
 
-FILENAME = 'tweets.txt'
+# FILENAME = 'tweets.txt'
+FILENAME = 'tweetsssssss.txt'
 WORD_DICT = {}
 
 
@@ -20,17 +22,19 @@ def count_freq_word(line):
 	"""
 	Count the frequency of the words
 	"""
-	word_ls = line[1].split(" ")  
+	word_ls = line[1].split(" ") 
+	store_time_freq_ls = []
 	for word in word_ls:
 
 		if word not in WORD_DICT:
 			if '@' in word:
 				pass
 			else:
-				WORD_DICT[word] = 1   
+				WORD_DICT[word] = 1
+
 		else:
 			WORD_DICT[word] += 1
-
+	
 
 def read_file(filename, word, mode='Word'):
 	"""
@@ -53,6 +57,12 @@ def read_file(filename, word, mode='Word'):
 			return count
 
 
+def preprocess_dict():
+	new_dict = {}
+	for k, v in WORD_DICT.items():
+		new_dict[k] = v
+
+	return new_dict
 
 def main():
 	# create args
@@ -68,11 +78,14 @@ def main():
 	if len(phrase) > 1:
 		# if phrase
 		count = read_file(FILENAME, word, mode='Phrase')
-		print(count)
+		print(f'The frequency of "{word}" : {count}')
 	else:
 		# if word
 		read_file(FILENAME, word, mode='Word')
-		print(WORD_DICT[word])
+		if word in WORD_DICT:
+			print(f'The frequency of {word} : {WORD_DICT[word]}')
+		else:
+			print(f'Cannot find the word "{word}"')
 	
 
 if __name__ == '__main__':
